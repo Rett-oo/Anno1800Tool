2961ee9bb8b0a0891d03cd37b55404a4856bf5ad
Rett-oo <106655967+Rett-oo@users.noreply.github.com>
2022-12-14 03:21:08 +0300

    create_window. Painevent, btn func

--- resources/settings.py
+++ resources/settings.py
@@ -1,7 +1,6 @@
-"""Doc."""
-
+"""CREATE SETTINGS WINDOW."""
 from PyQt5.QtWidgets import QGridLayout, QWidget, QVBoxLayout, \
-    QLabel, QMessageBox, QHBoxLayout, QSpacerItem, QSizePolicy  # noqa
+    QLabel, QHBoxLayout
 from PyQt5.QtGui import QPainter, QPixmap
 from PyQt5.QtCore import *  # noqa
 from PyQt5 import QtCore
@@ -12,7 +11,7 @@ class Widget_settings(QWidget):
     """Doc."""
 
     def __init__(self, parent):
-        """Doc."""
+        """INIT CLASS."""
         super(Widget_settings, self).__init__(parent)
 
         self.parent = parent
@@ -20,74 +19,70 @@ class Widget_settings(QWidget):
         self.pressing = False
         self.initSubWindow()
 
-    def paintEvent(self, event):
-        """PAINT EVENT."""
-        QWidget.paintEvent(self, event)
-
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.SmoothPixmapTransform)
-        painter.drawPixmap(self.rect(), QPixmap(
-            "E:/Python/Anno1800Tool/resources/images/assets/bg/bg_subwindow_postcard_gonesister_01_0.png"))  # noqa
-
     def initSubWindow(self):
-        """Doc."""
+        """CLASS M OBJECT."""
         self.setMinimumSize(700, 500)
         self.setMaximumSize(1100, 800)
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-
+        # main layout
         settings_layout = QGridLayout()
         settings_layout.setContentsMargins(90, 50, 50, 50)
         self.setLayout(settings_layout)
 
-        header_hbox = QHBoxLayout()
-        header_hbox.setContentsMargins(50, 0, 0, 0)
-        settings_layout.addLayout(header_hbox, 1, 1,
-                                  Qt.AlignHCenter, Qt.AlignTop)
+        def createheader() -> QHBoxLayout:  # header
+            header_hbox = QHBoxLayout()
+            header_hbox.setContentsMargins(50, 0, 0, 0)
 
-        settings_label = QLabel("Settings")
-        settings_label.setFont(QFont("Heuristica", 12))
-        settings_label.setAlignment(Qt.AlignHCenter)
-        header_hbox.addWidget(settings_label, alignment=Qt.AlignHCenter)
+            settings_label = QLabel("Settings")
+            settings_label.setFont(QFont("Heuristica", 12))
+            settings_label.setAlignment(Qt.AlignHCenter)
+            header_hbox.addWidget(settings_label, alignment=Qt.AlignHCenter)
 
-        spacer = QSpacerItem(10, 1, QSizePolicy.Expanding, QSizePolicy.Fixed)  # noqa
-        # header_hbox.addSpacerItem(spacer)
+            def createheader_btn(action: str) -> QAbstractButton:
+                btn = PushButton_HC(action)
+                btn.setMinimumSize(30, 30)
+                btn.setMaximumSize(30, 30)
+                header_hbox.addWidget(btn, alignment=Qt.AlignTop)
+                if action == "Hide":
+                    btn.clicked.connect(self.showMinimized)
+                elif action == "Exit":
+                    btn.clicked.connect(self.close)
+                return btn
 
-        hide_btn = PushButton_HC("Hide")
-        hide_btn.setMinimumSize(30, 30)
-        hide_btn.setMaximumSize(30, 30)
-        hide_btn.clicked.connect(self.showMinimized)
-        close_btn = PushButton_HC("Exit")
-        close_btn.setMinimumSize(30, 30)
-        close_btn.setMaximumSize(30, 30)
-        close_btn.setMaximumSize(30, 30)
-        close_btn.clicked.connect(self.close)
+            createheader_btn("Hide")
+            createheader_btn("Exit")
+            return header_hbox
 
-        header_hbox.addWidget(hide_btn, alignment=Qt.AlignTop)
-        header_hbox.addWidget(close_btn, alignment=Qt.AlignTop)
+        settings_layout.addLayout(createheader(),
+                                  1, 1, Qt.AlignHCenter, Qt.AlignTop)
 
-        mainBtn_vbox = QVBoxLayout()
-        mainBtn_vbox.setAlignment(Qt.AlignTop)
-        settings_layout.addLayout(mainBtn_vbox, 2, 1, Qt.AlignLeft)
+        def createsettings_area() -> QVBoxLayout:  # settings button
+            mainBtn_vbox = QVBoxLayout()
+            mainBtn_vbox.setAlignment(Qt.AlignTop)
 
-        settings1 = PushButton_Settings("Settings_1")
-        settings1.setMinimumSize(100, 35)
-        settings1.setMaximumSize(135, 55)
-        settings2 = PushButton_Settings("Settings_2")
-        settings2.setMinimumSize(100, 35)
-        settings2.setMaximumSize(135, 55)
-        settings3 = PushButton_Settings("Settings_3")
-        settings3.setMinimumSize(100, 35)
-        settings3.setMaximumSize(135, 55)
-        self.quit_btn = PushButton_Settings("Exit")
-        self.quit_btn.setMinimumSize(100, 35)
-        self.quit_btn.setMaximumSize(135, 55)
-        self.quit_btn.clicked.connect(self.exitApp)
+            def createsettings_btn(section: str) -> QAbstractButton:
+                btn = PushButton_Settings(section)
+                btn.setMinimumSize(100, 35)
+                btn.setMaximumSize(135, 55)
+                mainBtn_vbox.addWidget(btn, alignment=Qt.AlignTop)
+                if section == "Settings_1":
+                    pass
+                elif section == "Settings_2":
+                    pass
+                elif section == "Settings_3":
+                    pass
+                else:
+                    btn.clicked.connect(self.exitApp)
+                return btn
 
-        mainBtn_vbox.addWidget(settings1, alignment=Qt.AlignTop)
-        mainBtn_vbox.addWidget(settings2, alignment=Qt.AlignTop)
-        mainBtn_vbox.addWidget(settings3, alignment=Qt.AlignTop)
-        mainBtn_vbox.addWidget(self.quit_btn, alignment=Qt.AlignTop)
+            createsettings_btn("Settings_1")
+            createsettings_btn("Settings_2")
+            createsettings_btn("Settings_3")
+            createsettings_btn("Exit")
+            return mainBtn_vbox
+
+        settings_layout.addLayout(createsettings_area(), 2, 1, Qt.AlignLeft)
 
     def mousePressEvent(self, event):
         """MOUSE PRESS EVENT."""
@@ -104,6 +99,15 @@ class Widget_settings(QWidget):
     def mouseReleaseEvent(self, event):
         """MOUSE RELEASE EVENT."""
         self.pressing = False
+
+    def paintEvent(self, event):
+        """PAINT EVENT."""
+        QWidget.paintEvent(self, event)
+
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.SmoothPixmapTransform)
+        painter.drawPixmap(self.rect(), QPixmap(
+            "resources/images/assets/bg/bg_subwindow_postcard_gonesister_01_0.png"))  # noqa
 
     def exitApp(self):
         """Doc."""
